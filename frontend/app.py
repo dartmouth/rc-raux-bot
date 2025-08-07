41d4045e7795e9b90b1a611dcb5139156cc88f79
iasolb <ianspraguesolberg@gmail.com>
2025-08-07 08:51:42 -0400

    simplified config.toml & green theme css

--- frontend/app.py
+++ frontend/app.py
@@ -5,10 +5,10 @@ from rc_raux_bot.agents.interviewer import interviewer
 from langchain.schema.runnable.config import RunnableConfig
 from langchain_core.messages import HumanMessage
 
-@cl.on_chat_start
-async def on_chat_start():
-    session_id = cl.user_session.get("id")
-    await cl.Message(f"Welcome to Raux Bot! \n Session ID: {session_id} \n How can I help?").send()
+# @cl.on_chat_start
+# async def on_chat_start():
+#     session_id = cl.user_session.get("id")
+#     await cl.Message(f"Welcome to Raux Bot! \n Session ID: {session_id} \n How can I help?").send()
 
 @cl.set_starters
 async def set_starters():
@@ -16,17 +16,17 @@ async def set_starters():
         cl.Starter(
             label="HPC support",
             message="I need support with HPC",
-            icon="/public/logo_light.png",
+            icon="/public/logo_light.png"
             ),
         cl.Starter(
-            label="AI/Langchain Support",
+            label="AI/Langchain Support", 
             message="I need support with AI or langchain Dartmouth",
-            icon="/public/logo_light.png",
+            icon="/public/logo_light.png"
             ),
          cl.Starter(
             label="Job support",
             message="I need support with Storage (DartFS, Discovert Clusters, Slurm, etc...)",
-            icon="/public/logo_light.png",
+            icon="/public/logo_light.png"
             )
     ]
 
